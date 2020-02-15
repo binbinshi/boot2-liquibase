d708975c28722dc703742a2748188faef242e900
shibinbin <shibinbin@megvii.com>
2020-02-15 12:22:26 +0800

    first commit

---
+++ src/main/java/com/kobin/boot2liquibase/Boot2LiquibaseApplication.java
@@ -0,0 +1,13 @@
+package com.kobin.boot2liquibase;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Boot2LiquibaseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Boot2LiquibaseApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/kobin/boot2liquibase/Boot2LiquibaseApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kobin.boot2liquibase;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Boot2LiquibaseApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
